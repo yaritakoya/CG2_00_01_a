fe98c67d7a6ebb37fc01ef7230930c2ec25c45f4
yaritakoya <k024g0053@g.neec.ac.jp>
2025-04-15 12:59:04 +0900

    ex.logのファイルの追加、書き出し、読み込みの生成

--- main.cpp
+++ main.cpp
@@ -1,19 +1,30 @@
 #include <Windows.h>
 #include <cstdint>
+//ファイルやディレクトリに関する操作を行うライブラリ
+#include <filesystem>
+//ファイルに書いたり読んだりするライブラリ
+#include<fstream>
+//時間を扱うライブラリ
+#include<chrono>
 
 //ウィンドウプロシーシャ
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 	//メッセージに応じてゲーム固有の処理を行う
-	switch (msg){
+	switch (msg) {
 		//ウィンドウが破棄された
 	case WM_DESTROY:
 		//OSに対して、アプリの終了を伝える
 		PostQuitMessage(0);
-			return 0;
+		return 0;
 	}
-	
+
 	//標準のメッセージ処理を行う
 	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+void Log(std::ostream& os, const std::string& message) {
+	os << message << std::endl;
+	OutputDebugStringA(message.c_str());
 }
 
 //Windouwsアプリでのエントリーポイント(main関数)
@@ -57,12 +68,30 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		nullptr							//オプション
 	);
 
+	//ログのディレクトリを用意する
+	std::filesystem::create_directory("logs");
+	//現在時刻を取得する(UTC時刻)
+	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+	//ログファイルの名前にコンマ何秒はいらないので、削って秒にする
+	std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>
+		nowSeconds = std::chrono::time_point_cast<std::chrono::seconds>(now);
+	//日本時間(PCの設定時間)に変換
+	std::chrono::zoned_time localTime{ std::chrono::current_zone(),nowSeconds };
+	//formatを使って年月日_時分秒の文字列に変換
+	std::string dateString = std::format("{:%Y%m%d_%H%M%S}", localTime);
+	//時刻を使ってファイル名を決定
+	std::string logFilePath = std::string("logs/") + dateString + "log";
+	//ファイルを作って書き込み準備
+	std::ofstream logStream(logFilePath);
+
+
+
 	//ウィンドウを表示する
 	ShowWindow(hwnd, SW_SHOW);
 
 	MSG msg{};
 	//ウィンドウの×ボタンが押されるまでループ
-	while (msg.message!=WM_QUIT){
+	while (msg.message != WM_QUIT) {
 		//Windowにメッセージが来てたら最優先で処理させる
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
 			TranslateMessage(&msg);
